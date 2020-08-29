625d23364ef960379a870fbf13754f8096ad7474
Josh Zappone <zappone.josh@gmail.com>
2020-08-29 12:46:09 -0700

    gives each note unique id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,19 +12,33 @@ module.exports = function (app) {
 
 
 app.post('/api/notes', function (req, res) {
-    fs.readFile('./db/db.json', function(err,data) {
+    fs.readFile('./db/db.json', 'utf8', function(err,data) {
         if (err) console.log("Error reading database\n" + err);
         const list = JSON.parse(data);
 
-        const post = {
-            id: list.length + 1,
-            title: req.body.title,
-            text: req.body.text
-        }
-        list.push(post);
-        fs.writeFile('./db/db.json', JSON.stringify(list), function(err){
-            if (err) console.log("Error writing to database\n" + err);
-            console.log("successfully saved update");
+        // adding counter to give every note a unique id (previously tried to assign by length but can run into errors)
+        fs.readFile('./db/counter.txt', 'utf8', function(err, data) {
+            if (err) throw err;
+            let noteCount = parseInt(data) + 1;
+
+            fs.writeFile('./db/counter.txt', JSON.stringify(noteCount), function(err) {
+                if (err) console.log("Error incrementing counter\n" + err);
+                // console.log("successfully counter incremented");
+            })
+
+            const post = {
+                id: noteCount,
+                title: req.body.title,
+                text: req.body.text
+            }
+            list.push(post);
+            fs.writeFile('./db/db.json', JSON.stringify(list), function(err){
+                if (err) console.log("Error writing to database\n" + err);
+                // console.log("successfully saved update");
+            });
+
+            
+
         })
     })
 
@@ -32,7 +46,7 @@ res.end()
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-    fs.readFile('./db/db.json', function(err,data) {
+    fs.readFile('./db/db.json', 'utf8', function(err,data) {
         if (err) console.log("Error reading database\n" + err);
         const list = JSON.parse(data);
 
